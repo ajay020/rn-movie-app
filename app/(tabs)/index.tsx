020ec8493268601f7ceb040242560ec0d38af5dc
ajay <ajayjarwal44@gmail.com>
2025-11-16 16:59:35 +0530

    fetch and display movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,84 @@
-import { Text, View } from "react-native";
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useRouter } from "expo-router";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError
+  } = useFetch(() => fetchMovies({ query: "" }));
+
+
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-100">Welcome.</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="absolute w-full z-0"
+        resizeMode="cover"
+      />
+
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        <View className=" flex-1 mt-5" >
+
+          {
+            moviesLoading ? (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="mt-10 self-center"
+              />
+            ) : moviesError ? (
+              <Text>Error: {moviesError?.message}</Text>
+            ) : (
+              <View className="flex-1 justify-center items-center ">
+                <SearchBar
+                  placeholder="Search for movies"
+                  value=""
+                  onChangeText={() => { }}
+                  onPress={() => router.push("/search")}
+                />
+
+                <FlatList
+                  showsHorizontalScrollIndicator={false}
+                  className="pb-32 mt-2"
+                  data={movies}
+                  numColumns={3}
+                  columnWrapperStyle={{
+                    justifyContent:"flex-start",
+                    gap:20,
+                    paddingRight: 5,
+                    marginBottom: 10
+                  }}
+                  scrollEnabled={false}
+                  contentContainerStyle={{
+                    gap: 2,
+                  }}
+                  renderItem={({ item, index }) => (
+                    <MovieCard {...item} />
+                  )}
+                  keyExtractor={(item) => item.id.toString()}
+                  ItemSeparatorComponent={() => <View className="w-4" />}
+                />
+              </View>
+            )
+          }
+        </View>
+
+      </ScrollView>
+
     </View>
   );
 }
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,7 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Text, View } from 'react-native';
 
 const Search = () => {
+  const originalUrl =
+    'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
+
+  const proxyUrl =
+    "https://api.allorigins.win/raw?url=" + encodeURIComponent(originalUrl);
+
+  const fetchTestData = async () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTczODY3OTRkMDQ5MTZlY2Q4Mzk5MTk5OTNkMTFlOCIsIm5iZiI6MTc2MzE5Nzc5Ny4zOTEsInN1YiI6IjY5MTg0MzY1NjRhMzJiMjE2MjRjOWYxMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-j-ZUW1lWfUrLSFpPQ0UueVy-enL3jclEM-3zqgiGRw'
+      }
+    };
+
+    try {
+      console.log("TEST DATA FETCHING......")
+      const response = await fetch(originalUrl, options)
+      console.log("TEST RESPONSE", response);
+
+      const json = await response.json();
+      console.log("TEST DATA", json);
+    } catch (error: any) {
+      console.log("Error-->", error?.message)
+    }
+  }
+
+  useEffect(() => {
+    console.log("Test data fetching:");
+
+    fetchTestData()
+
+  }, []);
+
   return (
     <View>
       <Text>Search</Text>
